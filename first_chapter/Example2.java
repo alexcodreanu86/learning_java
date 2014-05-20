1050fc249a19b8709a26a8dade80a0145136cbe2
Alexandru Codreanu <alexcodreanu86@yahoo.com>
2014-05-20 12:53:17 -0500

    second program in chapter 1

---
+++ first_chapter/Example2.java
@@ -0,0 +1,18 @@
+/* 
+  This demonstrates a variable. 
+  Call this file Example2.java
+ */
+class Example2 {
+  public static void main(String args[]) {
+    int var1; // this declares a variable of type integer
+    int var2; // this declares another integer variable
+
+    var1 = 1024;
+    System.out.println("var1 contains " + var1);
+
+    var2 = var1 / 2;
+
+    System.out.print("var 2 contains var1 / 2: ");
+    System.out.println(var2);
+  }
+}
